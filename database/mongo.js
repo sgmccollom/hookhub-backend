d84145598793a2525a27c98b823dbc742b3679b1
Stephanie McCollom <stephanie.g.malek@gmail.com>
2024-01-02 19:00:02 -0600

    Update mongo.js

--- database/mongo.js
+++ database/mongo.js
@@ -1,10 +1,6 @@
-// using Mai's mongo Atlas here. may change later
-// const username = 'team-8'
-// const password = "secret12345"
-// const collectionName = "Logs"
-// const mongoURL = `mongodb+srv://${username}:${password}@cluster0.ze8g2xm.mongodb.net/${collectionName}?retryWrites=true&w=majority`
 
-const MONGODB_URI = "mongodb+srv://sgmcm-fullstack:U2r8OXdPl8EJBmAR@cluster0.tvfzl43.mongodb.net/requestbin?retryWrites=true&w=majority";
+
+const MONGODB_URI;
 // Connect to mongo
 // const config = require("../utils/config");
 const mongoose = require("mongoose");
